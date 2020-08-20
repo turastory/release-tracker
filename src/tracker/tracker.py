43b2557b630babf4fa9a47bf21868b2240471813
turastory <soldier4443@gmail.com>
2020-08-20 20:05:27 +0900

    :sparkles: Set up command line parser using docopt

--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -1,2 +1,24 @@
+"""
+Test document.
+
+Usage:
+  track <start-tag-name> [<target-tag-name>]
+  track (-h | --help)
+  track --version
+
+Options:
+  -h --help       Show this screen.
+  --version       Show version.
+  --regex=<regex> Regular expression to filter out commits
+
+"""
+from docopt import docopt
+
+
 def func(x):
     return x + 1
+
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version="1.0.0")
+    print(arguments)
