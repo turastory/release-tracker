0f0fe583151d64aa3f9192c8bf8e649cf4eff138
turastory <soldier4443@gmail.com>
2020-08-21 10:36:07 +0900

    :sparkles: Add list_commits() function

--- src/tracker/gitutil.py
+++ src/tracker/gitutil.py
@@ -14,3 +14,14 @@ def find_tag(repo, pattern):
     """
     p = re.compile(pattern)
     return next((x for x in repo.tags if p.match(x.name)), None)
+
+
+def list_commits(repo, start, end):
+    """List all commits between start and end revisions
+
+    Args:
+        repo (Repo)
+        start (string)
+        end (string)
+    """
+    return list(repo.iter_commits(f"{start}...{end}"))
--- tests/test_gitutil.py
+++ tests/test_gitutil.py
@@ -22,3 +22,20 @@ def test_find_tag(tmpdir):
     repo.create_tag("abcd")
     assert gitutil.find_tag(repo, "test").name == "test"
     assert gitutil.find_tag(repo, "a.*").name == "abcd"
+
+
+def test_list_commits(tmpdir):
+    repo = setup_git_repo(tmpdir)
+    commit_new_file(repo, "first", "first commit")
+    tag1 = repo.create_tag("first_tag")
+    commit_new_file(repo, "second", "second commit")
+    tag2 = repo.create_tag("second_tag")
+    commit_new_file(repo, "third", "third commit")
+    assert [x.message
+            for x
+            in gitutil.list_commits(repo, "HEAD", tag1.path)
+            ] == ["third commit", "second commit"]
+    assert [x.message
+            for x
+            in gitutil.list_commits(repo, "HEAD", tag2.path)
+            ] == ["third commit"]
